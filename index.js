082f68c2170f6763af7c9767f0a8cd56dc22d7d8
Charlotte Walker <charlielizzy@me.com>
2022-03-25 15:35:14 +0000

    up to task 9

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 class Board {
-  constructor(state = ["", "", "", "", "", "", "", "", ""]) {
+  constructor(state = ["O", "X", "O", "X", "O", "O", "X", "O", "X"]) {
     this.state = state;
   }
 
@@ -24,13 +24,13 @@ class Board {
   }
 
   isEmpty() {
-    let newArray = [];
+    let emptyArray = [];
     this.state.forEach((item) => {
       if (item === "X" || item === "O") {
-        newArray.push(item);
+        emptyArray.push(item);
       }
     });
-    if (newArray.length > 0) {
+    if (emptyArray.length > 0) {
       return false;
     } else {
       return true;
@@ -38,26 +38,82 @@ class Board {
   }
 
   isFull() {
-    let newArray = [];
+    let fullArray = [];
     this.state.forEach((item) => {
       if (item === "X" || item === "O") {
-        newArray.push(item);
+        fullArray.push(item);
       }
     });
-    if (newArray.length === 9) {
+    if (fullArray.length === 9) {
       return true;
     } else {
       return false;
     }
   }
 
-  addNewState(newXO, position) {
-    this.state[position] = newXO;
-    this.printBoard();
+  addNewState(newXO, index) {
+    if (newXO !== "X" && newXO !== "O") {
+      throw new Error("Invalid character - enter X or O");
+    }
+    if (this.state[index] !== "") {
+      throw new Error("This space is full");
+    } else {
+      this.state[index] = newXO;
+      this.printBoard();
+      if (index > 8 || index < 0) {
+        throw new Error("Position does not exist");
+      }
+      if (isNaN(index) === true) {
+        throw new Error("Position does not exist");
+      }
+    }
+  }
+
+  availableIndex() {
+    let availableArray = [];
+    this.state.forEach((item, index) => {
+      if (item === "") {
+        availableArray.push(index);
+      }
+    });
+    console.log(availableArray);
+  }
+
+  gameWon() {
+    let gameWon;
+    if (((this.state[0] === this.state[1]) === this.state[2]) !== "") {
+      gameWon = true;
+    } else if (((this.state[3] === this.state[4]) === this.state[5]) !== "") {
+      gameWon = true;
+    } else if (((this.state[6] === this.state[7]) === this.state[8]) !== "") {
+      gameWon = true;
+    } else if (((this.state[0] === this.state[3]) === this.state[6]) !== "") {
+      gameWon = true;
+    } else if (((this.state[1] === this.state[4]) === this.state[7]) !== "") {
+      gameWon = true;
+    } else if (((this.state[2] === this.state[5]) === this.state[8]) !== "") {
+      gameWon = true;
+    } else if (((this.state[0] === this.state[4]) === this.state[8]) !== "") {
+      gameWon = true;
+    } else if (((this.state[2] === this.state[4]) === this.state[6]) !== "") {
+      gameWon = true;
+    } else {
+      gameWon = false;
+    }
+    return gameWon;
+    // else (gameWon === true) {
+    //   return "game won";
+    //   // } else if (gameWon === false) {
+    //   //   return "game draw";
+    // }
   }
 }
 
 const board = new Board();
 
-board.addNewState("X", 6);
-// board.printBoard();
+// board.addNewState("X", 4);
+board.printBoard();
+
+board.availableIndex();
+
+console.log(board.gameWon());
